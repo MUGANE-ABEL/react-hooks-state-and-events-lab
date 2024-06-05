87740b8c68e8f41d203af595739ef688c509ae13
Mugane Abel <muganeabel@gmail.com>
2024-06-05 11:28:18 +0300

    suiiii

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+const [isInTheCart, setIsInTheCart]= useState(true)
+
+function handleAdding(){
+  setIsInTheCart((isInTheCart)=> !isInTheCart)
+}
+
+const adding= isInTheCart ? "" : "in-cart"
+const buttonApp = isInTheCart ? "add" : "remove"
+
+
   return (
-    <li className="">
+    <li className= {adding}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonApp} onClick={handleAdding} >{isInTheCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
